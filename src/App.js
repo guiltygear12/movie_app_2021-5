2f6295ab3687739070f0f733600ab56a3ba08ad4
park <mylove700757@gmail.com>
2021-09-15 16:52:24 +0900

    09.15 수업내용정리 03-4 내용까지 정리 완료 4장 내용 진행중

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
+
+const foodLike = [
+  {
+    id: 1,
+    name:"Neng-Myeon",
+    image:"https://thejincook.com/upload/menu_01/2019_05_10/hero_6iioG_2019_05_10_16_04_36.jpg"
+  },
+  {
+    id: 2,
+    name:"TTek-Bokk-KKi",
+    image:"https://t1.daumcdn.net/cfile/tistory/99DDDC445BD839460C"
+  },
+  {
+    id: 3,
+    name:"Gan-JJa-Jang",
+    image:"http://ojsfile.ohmynews.com/STD_IMG_FILE/2016/1214/IE002069160_STD.jpg"
+  }
+]
+
 function App() {
   return (
-  <div>Hello world!!!!!!!!!</div>
+    <div>
+      {foodLike.map(dish=>(
+        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+      ))}  
+    </div>
   )
 }
+
+function Food({name,picture}){
+  return (
+    <div>
+      <h1>I Like {name}!</h1>
+      <img src={picture} alt={name} />
+    </div>
+  )
+}
+
+
+
 export default App
---
+++ src/Potato.js
@@ -0,0 +1,5 @@
+function Potato(bar){
+    return <h1>{bar.customer} 고객님, 안녕하세요!</h1>
+}
+
+export default Potato
