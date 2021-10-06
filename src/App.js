6cf609628e5327f1734571f100b12ab8c3265ec9
park <mylove700757@gmail.com>
2021-10-06 14:32:26 +0900

    5장 수업진행 완료

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import {Component} from 'react'
+import React from 'react'
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    console.log('hello')
-  }
+class App extends React.Component{
   
   state={
-    count:0,
+    isLoading:true,
+    movies:[],
   }
-  
-add=()=>{
-  this.setState(current=>(
-      {count:current.count+1
-    }))
-}
 
-minus=()=>{
-  this.setState(current=>(
-    {count:current.count-1
-  }))
-}
-
-componentDidMount(){
-  console.log('component rendered')
-}
-componentDidUpdate(){
-  console.log("i' just updated")
-}
-componentWillUnmount(){
-  console.log("Good bye, cruel world")
-}
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading:false})
+    },6000)
+  }
 
   render(){
-    console.log("i'm rendering")
+    const{ isLoading } = this.state;
     return(
       <div>
-        <h1>THE Number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {isLoading ? '데이터를 불러오는 중 입니다...':'데이터 불러오기 완료!'}
       </div>
     )
   }
